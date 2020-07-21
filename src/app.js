0ca22af3ed1d82827fb20e4720319f82daa4bbd5
gabriel carrasco <gabriel136@hotmail.it>
2020-07-21 11:48:46 +0200

    add jquery for mobile menu toggle

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 import style from './styles/style.scss';
+
+import $ from 'jquery';
+
+$(document).ready(function(){
+//  $nav = $('.nav');
+//  $toggleCollapse = $('.toggle-collapse');
+  
+  /* click event on toggle menu */
+  $('.toggle-collapse').click(function(){
+    $('.nav').toggleClass('collapse');
+  })
+});
